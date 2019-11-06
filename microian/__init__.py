1118fb579f14baf04938a00316add0fc82291b86
Ian Gomez <ian.nicolasgomez@gmail.com>
2019-11-05 23:27:42 -0500

    Modified init to always allow outside connections (host=...) Changed a few folders to match flask best practice Created a welcome html that's served statically

--- microian/__init__.py
+++ microian/__init__.py
@@ -1,15 +1,14 @@
-from flask import Flask
+from flask import (Flask, render_template)
 app = Flask(__name__)
 
-
 @app.route('/')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/hello')
 def hello():
     return "Hello World!"
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
-
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
