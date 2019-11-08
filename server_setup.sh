aeb15163c80cf4468e4badd18eed07bffccce451
Ian Gomez <ian.nicolasgomez@gmail.com>
2019-11-08 18:37:27 -0500

    Renamed files

--- server_setup.sh
+++ server_setup.sh
@@ -3,13 +3,13 @@ adduser ian;
 usermod -aG sudo ian;
 rsync --archive --chown=ian:ian ~/.ssh /home/ian;
 
-# firewall
-ufw allow OpenSSH;
-ufw enable -y;
-
 # nginx
 sudo apt update -y;
 sudo apt install -y nginx;
+
+# firewall
+sudo ufw allow OpenSSH;
+sudo ufw enable -y;
 sudo ufw allow 'Nginx HTTP';
 sudo ufw allow 5000;
 
@@ -19,9 +19,9 @@ sudo apt-get -y upgrade;
 sudo apt-get install -y python3-pip python3-venv build-essential libssl-dev libffi-dev python-dev;
 
 # new metrics for droplets
-sudo apt-get purge do-agent;
+sudo apt-get -y purge do-agent;
 curl -sSL https://repos.insights.digitalocean.com/install.sh | sudo bash;
-sudo apt autoremove;
+sudo apt autoremove -y;
 
 # set up git
 git config --global user.email "ian.nicolasgomez@gmail.com";
@@ -30,4 +30,4 @@ git config --global user.name "Ian Gomez";
 # set up project
 python3 -m venv venv;
 source venv/bin/activate;
-pip install -r stable-req.txt
+pip install -r stable-req.txt;
