ba6657da4d9fa37f6b60f6d0dc6f9ef800d8d24f
Ian Gomez <ian.nicolasgomez@gmail.com>
2019-11-08 02:04:27 -0500

    Update droplet and requirements

--- droplet_setup.sh
+++ droplet_setup.sh
@@ -8,29 +8,26 @@ ufw allow OpenSSH;
 ufw enable -y;
 
 # nginx
-sudo apt update;
-sudo apt install nginx;
+sudo apt update -y;
+sudo apt install -y nginx;
 sudo ufw allow 'Nginx HTTP';
+sudo ufw allow 5000;
 
 # install python essentials
 sudo apt-get update;
 sudo apt-get -y upgrade;
 sudo apt-get install -y python3-pip python3-venv build-essential libssl-dev libffi-dev python-dev;
-pip3 install gunicorn flask;
 
 # new metrics for droplets
 sudo apt-get purge do-agent;
 curl -sSL https://repos.insights.digitalocean.com/install.sh | sudo bash;
+sudo apt autoremove;
 
 # set up git
 git config --global user.email "ian.nicolasgomez@gmail.com";
 git config --global user.name "Ian Gomez";
-git clone https://github.com/iangomez/websitedev.git;
 
 # set up project
-cd websitedev;
 python3 -m venv venv;
 source venv/bin/activate;
-pip install wheel;
-pip install uwsgi flask;
-sudo ufw allow 5000;
+pip install -r stable-req.txt
