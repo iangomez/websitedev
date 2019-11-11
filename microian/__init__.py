6a9f7fdfd9836ef5bc0c21b00fb174682b520397
Ian Gomez <ian.nicolasgomez@gmail.com>
2019-11-11 14:25:09 -0500

    Last commit before getting full database running

--- microian/__init__.py
+++ microian/__init__.py
@@ -4,19 +4,19 @@ from flask import Flask
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
-    # app.config.from_mapping(
-    #     # a default secret that should be overridden by instance config
-    #     SECRET_KEY="dev",
-    #     # store the database in the instance folder
-    #     DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
-    # )
+    app.config.from_mapping(
+        # a default secret that should be overridden by instance config
+        SECRET_KEY="dev",
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+    )
 
-    # if test_config is None:
-    #     # load the instance config, if it exists, when not testing
-    #     app.config.from_pyfile("config.py", silent=True)
-    # else:
-    #     # load the test config if passed in
-    #     app.config.update(test_config)
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.update(test_config)
 
     # ensure the instance folder exists
     try:
@@ -24,19 +24,15 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route("/hello")
-    def hello():
-        return "Hello, World!"
-
     # # register the database commands
-    # from flaskr import db
+    from microian import db
 
-    # db.init_app(app)
+    db.init_app(app)
 
     # apply the blueprints to the app
-    from microian import blog
+    from microian import auth, blog
 
-    # app.register_blueprint(auth.bp)
+    app.register_blueprint(auth.bp)
     app.register_blueprint(blog.bp)
 
     # make url_for('index') == url_for('blog.index')
