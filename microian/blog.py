30703c6b2e43a1937bfbfc83c8e303b83fcebe97
Ian Gomez <ian.nicolasgomez@gmail.com>
2019-11-11 14:45:22 -0500

    Need to init-db in the command line

--- microian/blog.py
+++ microian/blog.py
@@ -18,8 +18,9 @@ def index():
     """Show all the posts, most recent first."""
     db = get_db()
     posts = db.execute(
-        "SELECT * FROM sqlite_master where type='table'"
-
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " ORDER BY created DESC"
     ).fetchall()
     return render_template("blog/index.html", posts=posts)
 
