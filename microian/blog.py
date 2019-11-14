63cfeed9d065e9a5615317e674aaf21c36402263
Ian Gomez <ian.nicolasgomez@gmail.com>
2019-11-13 23:42:32 -0500

    Fixed up the login, register formatting. Fixed post creation with rich html text editing. Added a db to the git for now.

--- microian/blog.py
+++ microian/blog.py
@@ -18,7 +18,7 @@ def index():
     """Show all the posts, most recent first."""
     db = get_db()
     posts = db.execute(
-        "SELECT p.id, title, body, created, author_id, username"
+        "SELECT p.id, title, body, tags, created, author_id, username"
         " FROM post p JOIN user u ON p.author_id = u.id"
         " ORDER BY created DESC"
     ).fetchall()
@@ -40,7 +40,7 @@ def get_post(id, check_author=True):
     post = (
         get_db()
         .execute(
-            "SELECT p.id, title, body, created, author_id, username"
+            "SELECT p.id, title, body, tags, created, author_id, username"
             " FROM post p JOIN user u ON p.author_id = u.id"
             " WHERE p.id = ?",
             (id,),
@@ -64,6 +64,7 @@ def create():
     if request.method == "POST":
         title = request.form["title"]
         body = request.form["body"]
+        tags = request.form["tags"]
         error = None
 
         if not title:
@@ -74,8 +75,8 @@ def create():
         else:
             db = get_db()
             db.execute(
-                "INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)",
-                (title, body, g.user["id"]),
+                "INSERT INTO post (title, body, tags, author_id) VALUES (?, ?, ?, ?)",
+                (title, body, tags, g.user["id"]),
             )
             db.commit()
             return redirect(url_for("blog.index"))
@@ -92,6 +93,7 @@ def update(id):
     if request.method == "POST":
         title = request.form["title"]
         body = request.form["body"]
+        tags = request.form["tags"]
         error = None
 
         if not title:
